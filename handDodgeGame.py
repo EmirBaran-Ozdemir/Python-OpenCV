51cb8f45233b991b93662a0140e85d5d03bd5560
lordwile <lordwilee@outlook.com>
2022-08-14 16:02:13 +0300

    update220814_1602

--- handDodgeGame.py
+++ handDodgeGame.py
@@ -6,25 +6,24 @@ import time
 ######################
 wCam, hCam = 1280, 720
 xr, yr = 0, 0
-xSpeed, ySpeed = 20, 20
 textCounter = 0
 text = "Game Starting"
 whiteColor = (255, 255, 255)
 blackColor = (0, 0, 0)
 difficultyDefinition = True
-
 ######################
 
 
-def createMovingBox(xr, yr, xSpeed, ySpeed):
-    cv2.rectangle(img, (xr, yr), (xr + 40, yr + 40), whiteColor, cv2.FILLED)
+def createMovingBox(img, xr, yr, xSpeed, ySpeed, color, wCam, hCam):
+    cv2.rectangle(img, (xr, yr), (xr + xSize, yr + ySize), color, cv2.FILLED)
     xr = xr + xSpeed
     yr = yr + ySpeed
-
-    if xr >= wCam - 40 or xr == 0:
+    if xr >= wCam - xSize or xr == 0:
         xSpeed = xSpeed * -1
-    if yr >= hCam - 40 or yr == 0:
+    if yr >= hCam - ySize or yr == 0:
         ySpeed = ySpeed * -1
+
+    return xr, yr, xSpeed, ySpeed
 
 
 # Video Capture
@@ -36,24 +35,35 @@ detector = htm.handDetector(modelComplex=0, maxHands=1, detectionCon=0.9, trackC
 # imgCanvas = np.zeros((720, 1280, 3), np.uint8)
 print("Choose difficulty\n Difficulty should be between 1 and 3")
 difficulty = int(input())
+# Check if difficulty is valid
 while difficultyDefinition:
     if difficulty < 1 and difficulty > 3:
         print("Difficulty should be between 1 and 3")
         difficulty = int(input())
     else:
         difficultyDefinition = False
+# Set difficulty and variables
+if difficulty == 1:
+    xSize, ySize = 40, 40
+    xSpeed, ySpeed = 20, 20
+elif difficulty == 2:
+    xSize, ySize = 60, 60
+    xSpeed, ySpeed = 30, 30
+else:
+    xSize, ySize = 80, 80
+    xSpeed, ySpeed = 50, 50
 
 while True:
     success, img = camera.read()
-    img = detector.findHands(img)
-    lmList, bbox = detector.findPosition(img)
+    img = detector.findHands(img, draw=True)
+    lmList, bbox = detector.findPosition(img, draw=False)
     start = False
 
     # FLIP
     img = cv2.flip(img, 1)
     if len(lmList) != 0:
         x1, y1 = lmList[8][1], lmList[8][2]
-        fingers = detector.fingersUp(img=img, draw=True)
+        fingers = detector.fingersUp(img=img, draw=False)
         # Game start if index finger up and all other down
         if (
             fingers[0]
@@ -65,15 +75,9 @@ while True:
             start = True
         # Block moves you should try to escape from block
         if start == True:
-            if difficulty == 1:
-                createMovingBox(xr, yr, xSpeed, ySpeed)
-            elif difficulty == 2:
-                createMovingBox(xr, yr, xSpeed, ySpeed)
-                createMovingBox(xr, yr, xSpeed, ySpeed)
-            else:
-                createMovingBox(xr, yr, xSpeed, ySpeed)
-                createMovingBox(xr, yr, xSpeed, ySpeed)
-                createMovingBox(xr, yr, xSpeed, ySpeed)
+            xr, yr, xSpeed, ySpeed = createMovingBox(
+                img, xr, yr, xSpeed, ySpeed, whiteColor, wCam, hCam
+            )
 
     # FRAME RATE
     cTime = time.time()
